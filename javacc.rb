27dfb4dc2c889ec482c7a1ca7bc2dc2188163232
Kevin Driver <driverkt@users.noreply.github.com>
2018-12-12 15:56:36 -0800

    removing undent

--- javacc.rb
+++ javacc.rb
@@ -20,8 +20,7 @@ class Javacc < Formula
 
     bin.mkpath
 
-    ['javacc', 'jjtree', 'jjdoc'].each {|b| (bin+b).write script b}
-    ['javacc', 'jjtree', 'jjdoc'].each {|b| system "chmod +x " + (bin + b)}
+    ['javacc', 'jjtree', 'jjdoc'].each {|b| (bin+b).install script b}
 
   end
 
