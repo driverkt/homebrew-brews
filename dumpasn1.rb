a6ee1cace449093cede77cc4a66912f180adb434
Kevin Driver <driverkt@users.noreply.github.com>
2018-12-12 15:05:04 -0800

    updated version

--- dumpasn1.rb
+++ dumpasn1.rb
@@ -3,12 +3,12 @@ require 'formula'
 class Dumpasn1 < Formula
   homepage 'http://www.cs.auckland.ac.nz/~pgut001/dumpasn1.c'
   url 'https://www.cs.auckland.ac.nz/~pgut001/dumpasn1.c'
-  version '20170309'
-  sha256 '4b7c7d92c9593ee58c81019b2c3b7a7ee7450b733d38f196ce7560ee0e34d6b1'
+  version '20181212'
+  sha256 'd42b7fb8457b9282ee341429baaaaf0ef7b2310cb28fcf2fc41914e07e8b1370'
 
   resource "cfg_file" do
     url "https://www.cs.auckland.ac.nz/~pgut001/dumpasn1.cfg"
-    sha256 "8b0c25bb3a4608b4678d25c56965e26e7780c73715032e447a506514a0815201"
+    sha256 "94245ed185e2bdb94b00ba031bb67ab83980748626f532ee4565df886468f196"
   end
 
   def install
