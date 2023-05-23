e1ab268b063bed8a5bea3427b79ddbe2a6335285
Kevin Driver <largeprimes@icloud.com>
2023-05-23 15:29:34 -0500

    Update dumpasn1.rb

--- dumpasn1.rb
+++ dumpasn1.rb
@@ -4,7 +4,7 @@ class Dumpasn1 < Formula
   homepage 'http://www.cs.auckland.ac.nz/~pgut001/dumpasn1.c'
   url 'https://www.cs.auckland.ac.nz/~pgut001/dumpasn1.c'
   version '20210212'
-  sha256 'ed1eaafb0ad865b97738dfe0b0e5d602c76dc0cde4c0cee4cdcdd11c28f480e5'
+  sha256 '8ce8fdbf2e9b11d410b0ab4e44a6b3f89c27080113f051ec1054d230e050a0b8'
 
   resource "cfg_file" do
     url "https://www.cs.auckland.ac.nz/~pgut001/dumpasn1.cfg"
