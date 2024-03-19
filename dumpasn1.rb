e0e0dee477c3ad5162a1cd3943932b9254d081ca
Kevin Driver <largeprimes@icloud.com>
2024-03-19 16:42:09 -0500

    update version

--- dumpasn1.rb
+++ dumpasn1.rb
@@ -12,7 +12,7 @@ require 'formula'
 class Dumpasn1 < Formula
   homepage 'http://www.cs.auckland.ac.nz/~pgut001/dumpasn1.c'
   url 'https://www.cs.auckland.ac.nz/~pgut001/dumpasn1.c'
-  version '20230207'
+  version '20240319'
   sha256 '8ce8fdbf2e9b11d410b0ab4e44a6b3f89c27080113f051ec1054d230e050a0b8'
 
   resource "cfg_file" do
