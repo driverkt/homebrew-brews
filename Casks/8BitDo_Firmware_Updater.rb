6f62f73c60b15958b52c008d8127431b7dfddf77
Kevin Driver <driverkt@users.noreply.github.com>
2025-07-10 20:42:45 -0500

    8BitDo firmware updater cask

---
+++ Casks/8BitDo_Firmware_Updater.rb
@@ -0,0 +1,11 @@
+cask "8bitdo-firmware-updater" do
+  version "0.0.1"
+  sha256 "3bfc2523d66c51f7d16e06d4a9e130f58bf5f5ba2274ff140b13af6a833516dd"
+
+  url "https://download.8bitdo.com/Tools/FirmwareUpdater/8BitDo_Firmware_Updater_macOS.zip"
+  name "8BitDo Firmware Updater"
+  desc "8BitDo Firmware Updater"
+  homepage "https://support.8bitdo.com/firmware-updater.html"
+
+  app "8BitDo\\ Firmware\\ Updater.app"
+end
